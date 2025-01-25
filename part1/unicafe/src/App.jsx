662df6879311e75c5c57b6bbbe64616b0a6100c9
nikolairoshchin <99800316+nikolairoshchin@users.noreply.github.com>
2025-01-25 13:42:27 +0200

    exercise 1.10 added

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,28 +8,32 @@ const Button = ({onClick, text}) => {
   )
 }
 
-const Statistics = ({good, neutral, bad}) => {
-  const summa = good + neutral + bad
+const StatisticLine = ({text, value}) => {
+  return (
+      <p>{text} {value}</p>
+  )
+}
+
+const Statistic = ({ good, neutral, bad }) => {
+  let all = good + neutral + bad
   let average = 0
   let positive = 0
-  if (summa > 0) {
-    average = (good - bad)/summa
-    positive = good/summa*100
-  }
-  if (summa === 0) {
+  if ( all > 0 ) {
+    average = (good - bad)/all
+    positive = good/all*100 + ' %'
     return (
-      <p>No feedback given</p>
-    )
+    <>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive} />
+    </>
+   )
   }
   return (
-    <>
-      <p>good {good}</p>
-      <p>neutral {neutral} </p>
-      <p>bad {bad} </p>
-      <p>all {summa}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
-    </>
+    <p>No feedback given</p>
   )
 }
 
@@ -38,7 +42,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
   return (
     <div>
       <h1>give feedback</h1>
@@ -46,7 +50,7 @@ const App = () => {
       <Button onClick={()=>{setNeutral(neutral+1)}} text="neutral"/>
       <Button onClick={()=>{setBad(bad+1)}} text="bad"/>
       <h2>statistics</h2>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistic good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
