320023ac213edfea64bfc5c09d620fa55e86222d
nikolairoshchin <99800316+nikolairoshchin@users.noreply.github.com>
2025-01-29 10:25:26 +0200

    exercises 2.5 added

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,42 +1,4 @@
-const Header = ({name}) => {
-  return <h1>{name}</h1>
-}
-
-const Part =({part}) => {
-  return <p>{part.name} {part.exercises}</p>
-}
-
-const Content = ({parts}) => {
-  return ( 
-    <>
-      {parts.map(part =>
-        <Part key={part.id} part={part} />
-      )}
-    </>
-  )
-  
-}
-
-const Total = ({parts}) => {
-  const total = parts.reduce((accum, part) => accum + part.exercises, 0)
-  return (
-    <p>Number of exercises {total}</p>
-  )
-}
-
-const Course = ( {courses} ) => {
-  return (
-  <div>
-    {courses.map(course => 
-    <div key={course.id}>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-    )}
-  </div>
-  )
-}
+import Course from './Course'
 
 const App = () => {
   const courses = [
