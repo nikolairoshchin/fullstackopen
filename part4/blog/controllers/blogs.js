e1be30eb186345ea1b3f87ad4b9294c22af36c19
nikolairoshchin <99800316+nikolairoshchin@users.noreply.github.com>
2025-03-06 13:14:54 +0200

    exercise 4.17 added

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,8 +1,11 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+
     response.json(blogs)
 })
 
@@ -17,9 +20,20 @@ blogRouter.post('/', async (request, response) => {
         return response.status(400).json({ error: 'content missing' })
     }
 
-    const blog = new Blog(body)
+    const user = await User.findById(body.userId)
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user.id
+    })
   
     const result = await blog.save()
+    user.blogs = user.blogs.concat(result._id)
+    await user.save()
+
     response.status(201).json(result)
 })
 
